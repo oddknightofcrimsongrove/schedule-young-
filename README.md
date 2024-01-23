f62f814450da09888aca68d50140f204d67dd76d
oddknightofcrimsongrove <148396636+oddknightofcrimsongrove@users.noreply.github.com>
2024-01-22 22:27:11 -0500

    readme completed

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
 # schedule-young-
+Created a for Loop to attached each hour block
+created for loop for local storage
+created class for past, present, future.
+change color pallet
+create hour blocks from 9am-5pm
--- js/script.js
+++ js/script.js
@@ -8,7 +8,7 @@ const hour = dayjs().hour();
 console.log(hour);
 const today = dayjs();
 $('#currentDay').text(today.format('MMMM DD'));
-
+//created forLoop and attached to children of each hour block to set up listening event
 for(let i=9; i<18; i++){
   $('#hour-'+ i).children('.saveBtn').on('click', function(){
     const toDo = $('#hour-'+ i).children('.description').val();
@@ -16,7 +16,7 @@ for(let i=9; i<18; i++){
     localStorage.setItem("hour-"+ i, toDo);
   })
 }
-
+//created another for Loop for local storage to keep information stored in browser.
 for(let i = 9; i < 18; i++) {
 const toDo = localStorage.getItem("hour-" + i) || '';
 $('#hour-'+ i).children('.description').val(toDo);
